d244d23b1abadd7e980f0c5e95a7794256249b6f
Timothee BELHOMME <tbelhomm@e3r12p7.42.fr>
2020-12-01 15:30:11 +0100

    Add flag u

--- srcs/printf.c
+++ srcs/printf.c
@@ -6,7 +6,7 @@
 /*   By: tbelhomm </var/mail/tbelhomm>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 11:53:47 by tbelhomm          #+#    #+#             */
-/*   Updated: 2020/12/01 15:13:31 by tbelhomm         ###   ########.fr       */
+/*   Updated: 2020/12/01 15:29:17 by tbelhomm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -329,6 +329,98 @@ int		ft_display_flag_integer(int number, t_flags flag)
 	return (size);
 }
 
+static int	ft_intlen(int n)
+{
+	unsigned int	i;
+	unsigned int	n1;
+
+	if (n == 0)
+		return (2);
+	i = (n < 0 ? 2 : 1);
+	n1 = (n < 0 ? (unsigned int)-n : (unsigned int)n);
+	while (n1 > 9)
+	{
+		n1 /= 10;
+		i++;
+	}
+	return (++i);
+}
+
+static char	*ft_itoa_l(unsigned int n)
+{
+	char			*str;
+	unsigned int	i;
+
+	i = ft_intlen(n);
+	str = malloc(i * sizeof(char));
+	if (!str)
+		return (NULL);
+	if (n < 0)
+		str[0] = '-';
+	str[--i] = '\0';
+	while (n > 9)
+	{
+		str[--i] = n % 10 + '0';
+		n /= 10;
+	}
+	str[--i] = n + '0';
+	return (str);
+}
+
+int		ft_display_flag_unsigned_int(unsigned int number, t_flags flag)
+{	
+	char			*num;
+	char			*tmp;
+	int				size;
+
+	if (flag.point == 0 && number == 0)
+		return (ft_putchar_size(0, flag.taille + 1));
+	size = 0;
+	num = ft_itoa_l(number);
+	if (number < 0 && (flag.point >= 0 || flag.zero > 0))
+	{
+		if (flag.zero > 0 && flag.point == -1)
+			ft_putstr_len("-", 1);
+		tmp = ft_strtrim(num, "-");
+		free(num);
+		num = tmp;
+		flag.zero = 1;
+		flag.taille -= 1;
+		size++;
+	}
+	if (flag.moins == 1)
+	{
+		if (flag.point >= 0)
+		{
+			if (number < 0)
+				ft_putstr_len("-", 1);
+			size += ft_putspace_int(flag.point - 1, ft_strlen(num) - 1, 1);
+		}
+		size += ft_putstr_len(num, ft_strlen(num));
+	}
+	if (flag.point >= 0)
+	{
+		if ((size_t)flag.point < ft_strlen(num))
+			flag.point = ft_strlen(num);
+		flag.taille -= flag.point;
+		size += ft_putspace_int(flag.taille, 0, 0);
+	}
+	else
+		size += ft_putspace_int(flag.taille, ft_strlen(num), flag.zero);
+	if (flag.moins == 0)
+	{
+		if (flag.point >= 0)
+		{
+			if (number < 0)
+				ft_putstr_len("-", 1);
+			size += ft_putspace_int(flag.point - 1, ft_strlen(num) - 1, 1);
+		}
+		size += ft_putstr_len(num, ft_strlen(num));
+	}	
+	free(num);
+	return (size);
+}
+
 int		ft_display_flag(t_flags flag, va_list arg_list)
 {
 	int		size;
@@ -344,6 +436,8 @@ int		ft_display_flag(t_flags flag, va_list arg_list)
 		size = ft_display_flag_pourcentage(flag);
 	else if (flag.type == 'i' || flag.type == 'd')
 		size = ft_display_flag_integer(va_arg(arg_list, int), flag);
+	else if (flag.type == 'u')
+		size = ft_display_flag_unsigned_int(va_arg(arg_list, unsigned int), flag);
 	return (size);
 }
 
